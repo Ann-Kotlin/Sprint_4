b4b1cf8a8b51bd7d8661f92e388d4335ad63fe1a
Ann Plodunova <annkotlinskaya@gmail.com>
2024-12-13 23:05:48 +0300

    Example_4

--- Sprint4.py
+++ Sprint4.py
@@ -1,23 +1,22 @@
-# 129566869
+# 129887867
 def quantity(nums: list[int], limit: int) -> int:
     """Основной цикл программы"""
     # Сортировка списка по убыванию
     nums_sored = sorted(nums, reverse=True)
     count = 0
-    left = len(nums) - 1
-    for right in range(len(nums)):
-        if right == left:
-            break
-        elif right > left:
-            return count
-        elif nums_sored[right] + nums_sored[left] <= limit:
-            left -= 1
-        count += 1    
-    count += 1        
+    right = len(nums) - 1
+    left = 0
+    while left <= right:
+        if nums_sored[right] + nums_sored[left] <= limit:
+            right -= 1
+            left += 1
+        else:
+            left += 1
+        count += 1           
     return count
 
 
 if __name__ == '__main__':
-    nums = [int(x) for x in input().split()]
+    nums = [int(number) for number in input().split()]
     limit = int(input())
     print(quantity(nums, limit))
